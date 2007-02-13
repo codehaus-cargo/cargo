fa64dab73550269c94f98d38e845c4c59bac6207
Vincent Massol <vmassol@codehaus.org>
2007-02-13 22:08:47 +0000

    CARGO-487: Complete SVN reorganization
    * Moving testdata/ to resources/testdata/ as it needs to be shared among different modules

---
+++ resources/testdata/simple-ejb/src/main/java/org/codehaus/cargo/sample/testdata/ejb/SampleBean.java
@@ -0,0 +1,65 @@
+/*
+ * ========================================================================
+ * 
+ * Copyright 2005 Vincent Massol.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.testdata.ejb;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBException;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
+
+/**
+ * Test EJB to verify Cargo can deploy EJB JAR to containers which supports it. 
+ *
+ * @version $Id$
+ */
+public class SampleBean implements SessionBean
+{
+    public boolean isWorking() throws RemoteException
+    {
+        return true;
+    }
+
+    public void ejbCreate() throws CreateException
+    {
+        // Nothing to do here but this method is required by the spec.
+    }
+
+    public void ejbActivate() throws EJBException, RemoteException
+    {
+        // Nothing to do here but this method is required by the spec.
+    }
+
+    public void ejbPassivate() throws EJBException, RemoteException
+    {
+        // Nothing to do here but this method is required by the spec.
+    }
+
+    public void ejbRemove() throws EJBException, RemoteException
+    {
+        // Nothing to do here but this method is required by the spec.
+    }
+
+    public void setSessionContext(SessionContext arg0) throws EJBException, RemoteException
+    {
+        // Nothing to do here but this method is required by the spec.
+    }
+}
---
+++ resources/testdata/tomcat-context/src/main/java/org/codehaus/cargo/sample/testdata/tomcatcontext/TestServlet.java
@@ -0,0 +1,46 @@
+/*
+ * ========================================================================
+ * 
+ * Copyright 2004-2005 Vincent Massol.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.testdata.tomcatcontext;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Sample test Servlet used to verify that parameters passed in Tomcat's <code>context.xml</code>
+ * file are correctly passed to Tomcat when the WAR module is deployed. 
+ *
+ * @version $Id$
+ */
+public class TestServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, 
+        HttpServletResponse response) throws ServletException, IOException
+    {
+        String value = getServletContext().getInitParameter("testcontextxml");
+        PrintWriter out = response.getWriter();
+        out.print("Test value is [" + value + "]");
+        out.close();
+    }
+}
