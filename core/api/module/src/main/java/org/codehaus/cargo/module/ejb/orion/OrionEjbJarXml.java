d16971052c267a253df337624aff6c4368b9ceb8
Magnus Grimsell <unknown-email>
2007-10-17 13:16:20 +0000

    Fix for CARGO-493. Bug was reintroduced in a merge.

--- core/api/module/src/main/java/org/codehaus/cargo/module/ejb/orion/OrionEjbJarXml.java
+++ core/api/module/src/main/java/org/codehaus/cargo/module/ejb/orion/OrionEjbJarXml.java
@@ -1,20 +1,20 @@
-/* 
+/*
  * ========================================================================
- * 
+ *
  * Copyright 2005-2006 Vincent Massol.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ========================================================================
  */
 package org.codehaus.cargo.module.ejb.orion;
@@ -30,7 +30,7 @@ import org.jdom.Element;
 
 
 /**
- * Encapsulates the DOM representation of a oracle ejb deployment descriptor 
+ * Encapsulates the DOM representation of a oracle ejb deployment descriptor
  * <code>orion-ejb-jar.xml</code> to provide convenience methods for easy access and manipulation.
  *
  * @version $Id$
@@ -39,7 +39,7 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
 {
     /**
      * Constructor.
-     * 
+     *
      * @param document The DOM document representing the parsed deployment descriptor
      * @param type the type of the descriptor
      */
@@ -47,7 +47,7 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
     {
         super(document, type);
     }
-    
+
     /**
      * {@inheritDoc}
      * @see VendorEjbDescriptor#getFileName()
@@ -56,7 +56,7 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
     {
         return "orion-ejb-jar.xml";
     }
-    
+
     /**
      * {@inheritDoc}
      * @see VendorEjbDescriptor#getJndiName(EjbDef)
@@ -67,14 +67,21 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
         Element ejbElement = getEjb(ejb.getName());
         if (ejbElement != null)
         {
-            jndiName = ejbElement.getAttribute("location").getValue();
+            if (ejb.getLocal() != null)
+            {
+                jndiName = ejbElement.getAttribute("local-location").getValue();
+            }
+            if (jndiName == null)
+            {
+                jndiName = ejbElement.getAttribute("location").getValue();
+            }
         }
         return jndiName;
     }
 
     /**
      * Returns a specific ejb.
-     * 
+     *
      * @param ejbName the name of the ejb to get
      * @return the ejb or null if no ejb with that name exists
      */
@@ -85,20 +92,20 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
         {
             ejbElement = getEntityEjb(ejbName);
         }
-        
+
         return ejbElement;
     }
-    
+
     /**
      * Returns a specific ejb.
-     * 
+     *
      * @param ejbName the name of the ejb to get
      * @return the ejb or null if no ejb with that name exists
      */
     private Element getSessionEjb(String ejbName)
     {
         Element ejbElement = null;
-        
+
         Iterator names = getElements(new DescriptorTag(
             OrionEjbJarXmlType.getInstance(), "session-deployment", true));
         while (names.hasNext())
@@ -111,20 +118,20 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
                 break;
             }
         }
-        
+
         return ejbElement;
     }
-    
+
     /**
      * Returns a specific ejb.
-     * 
+     *
      * @param ejbName the name of the ejb to get
      * @return the ejb or null if no ejb with that name exists
      */
     private Element getEntityEjb(String ejbName)
     {
         Element ejbElement = null;
-        
+
         Iterator names = getElements(new DescriptorTag(
             OrionEjbJarXmlType.getInstance(), "entity-deployment", true));
         while (names.hasNext())
@@ -137,7 +144,7 @@ public class OrionEjbJarXml extends AbstractDescriptor implements VendorEjbDescr
                 break;
             }
         }
-        
+
         return ejbElement;
     }
 }
--- core/api/module/src/test/java/org/codehaus/cargo/module/ejb/orion/OracleEjbJarXmlTest.java
+++ core/api/module/src/test/java/org/codehaus/cargo/module/ejb/orion/OracleEjbJarXmlTest.java
@@ -1,20 +1,20 @@
-/* 
+/*
  * ========================================================================
- * 
+ *
  * Copyright 2005 Vincent Massol.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ========================================================================
  */
 package org.codehaus.cargo.module.ejb.orion;
@@ -35,7 +35,7 @@ public class OracleEjbJarXmlTest extends AbstractDocumentBuilderTest
 {
     /**
      * Tests the basic functionality of {@link OrionEjbJarXml.getJndiName}.
-     * 
+     *
      * @throws Exception If an unexpected error occurs
      */
     public void testGetJndiName() throws Exception
@@ -46,15 +46,15 @@ public class OracleEjbJarXmlTest extends AbstractDocumentBuilderTest
             + "    </session-deployment>"
             + "  </enterprise-beans>"
             + "</orion-ejb-jar>";
-         
+
         OrionEjbJarXml descr = OrionEjbJarXmlIo.parseOracleEjbJarXml(new ByteArrayInputStream(xml.getBytes()));
-       
+
         assertEquals("mycomp/MyEjb", descr.getJndiName(new EjbDef("MyEjb")));
     }
-    
+
     /**
      * Tests {@link OrionEjbJarXml.getJndiName}.
-     * 
+     *
      * @throws Exception If an unexpected error occurs
      */
     public void testGetJndiNameWithWrongName() throws Exception
@@ -65,15 +65,15 @@ public class OracleEjbJarXmlTest extends AbstractDocumentBuilderTest
             + "    </session-deployment>"
             + "  </enterprise-beans>"
             + "</orion-ejb-jar>";
-            
+
         OrionEjbJarXml descr = OrionEjbJarXmlIo.parseOracleEjbJarXml(new ByteArrayInputStream(xml.getBytes()));
 
         assertNull(descr.getJndiName(new EjbDef("foo")));
     }
-    
+
     /**
      * Tests {@link OrionEjbJarXml.getJndiName}.
-     * 
+     *
      * @throws Exception If an unexpected error occurs
      */
     public void testGetJndiNameOfEntityEjb() throws Exception
@@ -83,9 +83,30 @@ public class OracleEjbJarXmlTest extends AbstractDocumentBuilderTest
             + "    <entity-deployment name=\"MyEjb\" location=\"mycomp/MyEjb\"/>"
             + "  </enterprise-beans>"
             + "</orion-ejb-jar>";
-            
+
         OrionEjbJarXml descr = OrionEjbJarXmlIo.parseOracleEjbJarXml(new ByteArrayInputStream(xml.getBytes()));
 
         assertEquals("mycomp/MyEjb", descr.getJndiName(new EjbDef("MyEjb")));
+    }
+
+    /**
+     * Tests {@link OrionEjbJarXml.getJndiName}.
+     *
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testGetJndiNameOfLocalEjb() throws Exception
+    {
+        String xml = "<orion-ejb-jar>"
+            + "  <enterprise-beans>"
+            + "    <entity-deployment name=\"MyEjb\" location=\"mycomp/MyEjb\" local-location=\"localJndiName\"/>"
+            + "  </enterprise-beans>"
+            + "</orion-ejb-jar>";
+
+        OrionEjbJarXml descr = OrionEjbJarXmlIo.parseOracleEjbJarXml(new ByteArrayInputStream(xml.getBytes()));
+
+        EjbDef def = new EjbDef("MyEjb");
+        def.setLocal("sdf");
+        def.setLocalHome("laskfj");
+        assertEquals("localJndiName", descr.getJndiName(def));
     }
 }
