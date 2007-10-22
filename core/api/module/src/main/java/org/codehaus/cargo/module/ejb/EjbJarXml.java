1c9787c860da9c29e5c941ce629ad116eb47afe8
Magnus Grimsell <unknown-email>
2007-10-22 11:19:36 +0000

    CARGO-486 Fixed bug reitroduced in JDOM merge.

--- core/api/module/src/main/java/org/codehaus/cargo/module/ejb/EjbJarXml.java
+++ core/api/module/src/main/java/org/codehaus/cargo/module/ejb/EjbJarXml.java
@@ -1,20 +1,20 @@
-/* 
+/*
  * ========================================================================
- * 
+ *
  * Copyright 2005-2006 Vincent Massol.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ========================================================================
  */
 package org.codehaus.cargo.module.ejb;
@@ -30,7 +30,7 @@ import org.jdom.Element;
 
 
 /**
- * Encapsulates the DOM representation of a ejb deployment descriptor 
+ * Encapsulates the DOM representation of a ejb deployment descriptor
  * <code>ejb-jar.xml</code> to provide convenience methods for easy access and manipulation.
  *
  * @version $Id$
@@ -41,18 +41,18 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
      * List of vendor descriptors associated with this ejb-jar.xml.
      */
     private List vendorDescriptors = new ArrayList();
-    
+
     /**
      * Constructor.
-     * 
+     *
      * @param rootElement the root element of the document
      * @param type the document descriptor type
      */
-    public EjbJarXml(Element rootElement, DescriptorType type) 
+    public EjbJarXml(Element rootElement, DescriptorType type)
     {
         super(rootElement, type);
     }
-    
+
     /**
      * {@inheritDoc}
      * @see J2eeDescriptor#getFileName()
@@ -61,17 +61,17 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
     {
         return "ejb-jar.xml";
     }
-    
+
     /**
      * Associates a vendor specific descriptor with this web.xml.
-     * 
+     *
      * @param descr the vendor specific dexcriptor to associate
      */
     public void addVendorDescriptor(VendorEjbDescriptor descr)
     {
         this.vendorDescriptors.add(descr);
     }
-    
+
     /**
      * {@inheritDoc}
      * @see org.codehaus.cargo.module.J2eeDescriptor#getVendorDescriptors()
@@ -80,10 +80,10 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
     {
         return this.vendorDescriptors.iterator();
     }
-    
+
     /**
      * Returns all session ejbs in this descriptor.
-     * 
+     *
      * @return Iterator of Ssession objects representing all session ejbs
      */
     public final Iterator getSessionEjbs()
@@ -94,6 +94,7 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
         {
             Element sessionElement = (Element) sessionElements.next();
             Session session = new Session();
+            session.setId(sessionElement.getAttribute("id").getValue());
             session.setName(getChildText(sessionElement, EjbJarXmlTag.EJB_NAME));
             session.setLocal(getChildText(sessionElement, EjbJarXmlTag.LOCAL));
             session.setLocalHome(getChildText(sessionElement, EjbJarXmlTag.LOCAL_HOME));
@@ -102,10 +103,10 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
 
         return ejbs.iterator();
     }
-    
+
     /**
      * Returns all entity ejbs in this descriptor.
-     * 
+     *
      * @return Iterator of Entity objects representing all entity ejbs
      */
     public final Iterator getEntityEjbs()
@@ -116,6 +117,7 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
         {
             Element sessionElement = (Element) sessionElements.next();
             Entity entity = new Entity();
+            entity.setId(sessionElement.getAttribute("id").getValue());
             entity.setName(getChildText(sessionElement, EjbJarXmlTag.EJB_NAME));
             entity.setLocal(getChildText(sessionElement, EjbJarXmlTag.LOCAL));
             entity.setLocalHome(getChildText(sessionElement, EjbJarXmlTag.LOCAL_HOME));
@@ -124,10 +126,10 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
 
         return ejbs.iterator();
     }
-    
+
     /**
      * Return a specific session definition.
-     * 
+     *
      * @param name the name of the ejb.
      * @return the Session
      */
@@ -144,13 +146,13 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
                 break;
             }
         }
-        
+
         return result;
     }
 
     /**
      * Return a specific entity definition.
-     * 
+     *
      * @param name the name of the ejb.
      * @return the Entity
      */
@@ -167,7 +169,7 @@ public class EjbJarXml extends AbstractDescriptor implements J2eeDescriptor
                 break;
             }
         }
-        
+
         return result;
     }
 }
