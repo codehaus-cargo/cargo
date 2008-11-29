ae19c59d47dacc78326f482cad9be42f44233e09
Adrian Cole <ferncam1@gmail.com>
2008-11-29 20:28:54 +0000

    added support for weblogic10x standalone CARGO-625

---
+++ core/containers/weblogic/src/main/java/org/codehaus/cargo/container/weblogic/WebLogic10xStandaloneLocalConfiguration.java
@@ -0,0 +1,55 @@
+/* 
+ * ========================================================================
+ * 
+ * Copyright 2004-2005 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ========================================================================
+ */
+package org.codehaus.cargo.container.weblogic;
+
+/**
+ * WebLogic 10x standalone
+ * {@link org.codehaus.cargo.container.spi.configuration.ContainerConfiguration} implementation.
+ * WebLogic 10x is only slightly different to configure then WebLogic 9x.
+ * 
+ * @version $Id$
+ */
+public class WebLogic10xStandaloneLocalConfiguration extends
+    WebLogic9xStandaloneLocalConfiguration
+{
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see WebLogic9xStandaloneLocalConfiguration#WebLogic9xStandaloneLocalConfiguration(String)
+     */
+    public WebLogic10xStandaloneLocalConfiguration(String dir)
+    {
+        super(dir);
+        setProperty(WebLogicPropertySet.CONFIGURATION_VERSION, "10.0.1.0");
+        setProperty(WebLogicPropertySet.DOMAIN_VERSION, "10.0.1.0");
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Object#toString()
+     */
+    public String toString()
+    {
+        return "WebLogic 10x Standalone Configuration";
+    }
+
+}
