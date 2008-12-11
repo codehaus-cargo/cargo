caa0426ff230b4eda0a9bde4a22db29b1cb961ce
Adrian Cole <ferncam1@gmail.com>
2008-12-11 09:27:17 +0000

    CARGO-637: missing file

---
+++ core/containers/weblogic/src/main/java/org/codehaus/cargo/container/weblogic/internal/WebLogic9xStandaloneLocalConfigurationCapability.java
@@ -0,0 +1,48 @@
+/* 
+ * ========================================================================
+ * 
+ * Copyright 2004-2006 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ========================================================================
+ */
+package org.codehaus.cargo.container.weblogic.internal;
+
+import org.codehaus.cargo.container.property.DatasourcePropertySet;
+import org.codehaus.cargo.container.property.GeneralPropertySet;
+
+/**
+ * Capabilities of the WebLogic's
+ * {@link org.codehaus.cargo.container.weblogic.WebLogic9xStandaloneLocalConfiguration}
+ * configuration.
+ * 
+ * @version $Id: $
+ */
+public class WebLogic9xStandaloneLocalConfigurationCapability extends
+    WebLogicStandaloneLocalConfigurationCapability
+{
+
+    /**
+     * WebLogic 9x supports additional features not available in 8x.
+     */
+    public WebLogic9xStandaloneLocalConfigurationCapability()
+    {
+        super();
+
+        // it is possible to set server logging thresholds in WLS 9+
+        this.supportsMap.put(GeneralPropertySet.LOGGING, Boolean.TRUE);
+        // feature not implemented, yet
+        this.supportsMap.remove(DatasourcePropertySet.DATASOURCE);
+    }
+}
