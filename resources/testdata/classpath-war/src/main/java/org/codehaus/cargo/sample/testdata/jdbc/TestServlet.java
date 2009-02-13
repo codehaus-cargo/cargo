f59f0681a8858db5323343e991026a938193f705
Adrian Cole <ferncam1@gmail.com>
2009-02-13 18:30:00 +0000

    CARGO-649

---
+++ resources/testdata/classpath-war/src/main/java/org/codehaus/cargo/sample/testdata/jdbc/TestServlet.java
@@ -0,0 +1,54 @@
+/*
+ * ========================================================================
+ * 
+ * Copyright 2004-2005 Vincent Massol.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.testdata.jdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Sample test Servlet used to verify that jdbc datasource is deployed.
+ * 
+ * @version $Id:$
+ */
+public class TestServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            PrintWriter out = response.getWriter();
+            out.print("Got class!");
+            out.close();
+
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new ServletException(e);
+
+        }
+    }
+}
