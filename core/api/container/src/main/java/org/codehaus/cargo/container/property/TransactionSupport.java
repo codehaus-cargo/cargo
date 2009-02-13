20fb1b051958ac1a31b21e2c1756ca0f6e995f9e
Adrian Cole <ferncam1@gmail.com>
2009-02-13 14:04:07 +0000

    CARGO-644 add class to identify the support level of a Transaction

---
+++ core/api/container/src/main/java/org/codehaus/cargo/container/property/TransactionSupport.java
@@ -0,0 +1,93 @@
+/*
+ * ========================================================================
+ *
+ * Copyright 2005 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.container.property;
+
+/**
+ * Represents the transactional support of a resource, such as a <code>XA_TRANSACTION</code>.
+ * 
+ * @version $Id: $
+ */
+public final class TransactionSupport
+{
+
+    /**
+     * Indicates lack of transaction support.
+     */
+    public static final TransactionSupport NO_TRANSACTION =
+        new TransactionSupport("NO_TRANSACTION");
+
+    /**
+     * Indicates support of container-managed transactions.
+     */
+    public static final TransactionSupport LOCAL_TRANSACTION =
+        new TransactionSupport("LOCAL_TRANSACTION");
+
+    /**
+     * Indicates support of distributed transactions using XA protocol.
+     */
+    public static final TransactionSupport XA_TRANSACTION =
+        new TransactionSupport("XA_TRANSACTION");
+
+    /**
+     * string representation of the transaction support this object represents.
+     */
+    private final String transactionSupport;
+
+    /**
+     * This class is a JDK 1.4 typesafe enum. That is why this constructor is private.
+     * 
+     * @param transactionSupport transaction support designated.
+     */
+    private TransactionSupport(String transactionSupport)
+    {
+        this.transactionSupport = transactionSupport;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String toString()
+    {
+        return transactionSupport;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean equals(Object obj)
+    {
+        if (obj != null && obj instanceof TransactionSupport)
+        {
+            return toString().equals(((TransactionSupport) obj).toString());
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int hashCode()
+    {
+        return transactionSupport.hashCode();
+    }
+}
