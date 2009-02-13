82ea177041aea710d3fb41448cb1784b1dd1a5a6
Adrian Cole <ferncam1@gmail.com>
2009-02-13 14:09:28 +0000

    CARGO-644 add transaction type to datasource properties

--- core/api/container/src/main/java/org/codehaus/cargo/container/property/DatasourcePropertySet.java
+++ core/api/container/src/main/java/org/codehaus/cargo/container/property/DatasourcePropertySet.java
@@ -27,41 +27,59 @@ package org.codehaus.cargo.container.property;
 public interface DatasourcePropertySet
 {
     /**
-     * A property to encapsulate all the other datasource properties.  This is to get around
-     * cargo only passing strings around, instead of objects.
+     * A property to encapsulate all the other datasource properties. This is to get around cargo
+     * only passing strings around, instead of objects. <br>
      */
     String DATASOURCE = "cargo.datasource.datasource";
 
     /**
-     * The JNDI location that this datasource should be bound do (in the config file).
-     * Note that many application servers may prepend a context (typically
-     * <code>java:comp/env</code>) to this context.
+     * The JNDI location that this datasource should be bound do (in the config file). Note that
+     * many application servers may prepend a context (typically <code>java:comp/env</code>) to this
+     * context. <br>
      */
     String JNDI_LOCATION = "cargo.datasource.jndi";
 
     /**
-     * The type of the data source (typically <code>javax.sql.XADataSource</code> or
-     * <code>javax.sql.DataSource</code>).
+     * The type of the data source (typically <code>javax.sql.XADataSource</code>,
+     * <code>javax.sql.ConnectionPoolDataSource</code> or <code>javax.sql.DataSource</code>). <br>
      */
-    String DATASOURCE_TYPE = "cargo.datasource.type";
+    String CONNECTION_TYPE = "cargo.datasource.type";
 
     /**
-     * The class name of the JDBC driver.  Example: <code>org.hsqldb.jdbcDriver</code>.
+     * The transaction support of the data source. One of <code>NO_TRANSACTION</code>,
+     * <code>LOCAL_TRANSACTION</code> or <code>XA_TRANSACTION</code> <br>
+     */
+    String TRANSACTION_SUPPORT = "cargo.datasource.transactionsupport";
+
+    /**
+     * The class name of the Driver or XADataSource implementation clas. Example:
+     * <code>org.hsqldb.jdbcDriver/code>. <br>
      */
     String DRIVER_CLASS = "cargo.datasource.driver";
 
     /**
-     * The url to connect to the database.  Example: <code>jdbc:hsqldb:database/jiradb</code>.
+     * The url to connect to the database. Example: <code>jdbc:hsqldb:database/jiradb</code>. The
      */
     String URL = "cargo.datasource.url";
 
     /**
-     * The username to use when connecting to the database.
+     * The username to use when connecting to the database. <br>
      */
     String USERNAME = "cargo.datasource.username";
 
     /**
-     * The password to use when connecting to the database.
+     * The password to use when connecting to the database. <br>
      */
     String PASSWORD = "cargo.datasource.password";
+
+    /**
+     * Unique id to use in configuration files. <br>
+     */
+    String ID = "cargo.datasource.id";
+
+    /**
+     * Extra properties passed to the JDBC driver or datasource implementation. <br>
+     */
+    String CONNECTION_PROPERTIES = "cargo.datasource.properties";
+
 }
