1187a3dc7523c4c76764c474582c0bcac2c8a743
S. Ali Tokmen <nospam@alishomepage.com>
2010-04-28 07:23:08 +0000

    (CARGO-705) 'Username' attribute for Tomcat5x configuration

--- core/containers/tomcat/src/main/java/org/codehaus/cargo/container/tomcat/internal/AbstractTomcatConfigurationBuilder.java
+++ core/containers/tomcat/src/main/java/org/codehaus/cargo/container/tomcat/internal/AbstractTomcatConfigurationBuilder.java
@@ -93,7 +93,7 @@ public abstract class AbstractTomcatConfigurationBuilder extends AbstractConfigu
     {
         Properties parameters = new Properties();
         PropertyUtils.setPropertyIfNotNull(parameters, "url", ds.getUrl());
-        PropertyUtils.setPropertyIfNotNull(parameters, "user", ds.getUsername());
+        PropertyUtils.setPropertyIfNotNull(parameters, "username", ds.getUsername());
         PropertyUtils.setPropertyIfNotNull(parameters, "password", ds.getPassword());
 
         parameters.putAll(ds.getConnectionProperties());
