62092c9900618c2770b1ec8de228bcf995d31421
S. Ali Tokmen <nospam@alishomepage.com>
2010-05-13 09:22:14 +0000

    CARGO-703 : update documentation

--- core/containers/jboss/src/main/java/org/codehaus/cargo/container/jboss/internal/JBossStandaloneLocalConfigurationCapability.java
+++ core/containers/jboss/src/main/java/org/codehaus/cargo/container/jboss/internal/JBossStandaloneLocalConfigurationCapability.java
@@ -54,6 +54,9 @@ public class JBossStandaloneLocalConfigurationCapability extends
         this.propertySupportMap.put(GeneralPropertySet.PROTOCOL, Boolean.FALSE);
         this.propertySupportMap.put(ServletPropertySet.USERS, Boolean.FALSE);
         this.propertySupportMap.put(JBossPropertySet.CONFIGURATION, Boolean.TRUE);
+
+        this.defaultSupportsMap.put(JBossPropertySet.JBOSS_USER, Boolean.TRUE);
+        this.defaultSupportsMap.put(JBossPropertySet.JBOSS_PASSWORD, Boolean.TRUE);
     }
 
     /**
