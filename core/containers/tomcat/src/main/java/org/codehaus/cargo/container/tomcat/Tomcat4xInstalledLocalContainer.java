f8a3ce3ca0a687fce8609218e507161586ea3a6d
S. Ali Tokmen <nospam@alishomepage.com>
2010-07-01 23:44:56 +0000

    always wait in TC 4

--- core/containers/tomcat/src/main/java/org/codehaus/cargo/container/tomcat/Tomcat4xInstalledLocalContainer.java
+++ core/containers/tomcat/src/main/java/org/codehaus/cargo/container/tomcat/Tomcat4xInstalledLocalContainer.java
@@ -102,12 +102,8 @@ public class Tomcat4xInstalledLocalContainer extends AbstractCatalinaInstalledLo
     {
         super.waitForCompletion(waitForStarting);
 
-        if (!waitForStarting)
-        {
-            // Tomcat 4 stop is not synchronous, therefore sleep a bit after the
-            // CARGO ping component has stopped in order to allow some time for
-            // the server to stop completely
-            Thread.sleep(10000);
-        }
+        // Tomcat 4 start/stop is not synchronous, therefore sleep a bit after the CARGO ping
+        // component is OK in order to allow some time for the server to start/stop completely
+        Thread.sleep(10000);
     }
 }
