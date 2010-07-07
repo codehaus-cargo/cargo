03da61366af6f00c432697529dc4f44cdda189d9
S. Ali Tokmen <nospam@alishomepage.com>
2010-07-07 12:49:11 +0000

    [#CARGO-801] Create an IT for CARGO ANT, that covers CARGO-776 and CARGO-787 (and many more features)

---
+++ extensions/ant/samples/datasource-test/src/test/java/org/codehaus/cargo/sample/ant/DataSourceTest.java
@@ -0,0 +1,41 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus CARGO, copyright 2004-2010 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.ant;
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.codehaus.cargo.sample.java.PingUtils;
+import org.codehaus.cargo.util.log.Logger;
+import org.codehaus.cargo.util.log.SimpleLogger;
+
+public class DataSourceTest extends TestCase {
+
+    Logger logger = new SimpleLogger();
+
+    public void testDataSource() throws Exception {
+        final URL url = new URL("http://localhost:" + System.getProperty("http.port") + "/datasource-test/test");
+        final String expected = "Got connection!";
+
+        PingUtils.assertPingTrue(url.getPath() + " not started", expected, url, logger);
+    }
+
+}
