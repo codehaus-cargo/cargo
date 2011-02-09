781df5dfc628c47e6bfb695cdd4fbf077a4acecb
Anders Hammar <anders@hammar.net>
2011-02-09 19:44:51 +0000

    Refactored CARGO-871: Add tests for the Maven2 archetypes

--- extensions/maven2/archetypes/webapp-single-module/src/main/resources/archetype-resources/src/test/java/it/WebappTest.java
+++ extensions/maven2/archetypes/webapp-single-module/src/main/resources/archetype-resources/src/test/java/it/WebappTest.java
@@ -7,9 +7,18 @@ import java.net.HttpURLConnection;
 
 public class WebappTest extends TestCase
 {
+    private String baseUrl;
+    
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        String port = System.getProperty("servlet.port");
+        this.baseUrl = "http://localhost:" + port + "/${artifactId}";
+    }
+
     public void testCallIndexPage() throws Exception
     {
-        URL url = new URL("http://localhost:8080/${artifactId}");
+        URL url = new URL(this.baseUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.connect();
         assertEquals(200, connection.getResponseCode());
