3998173649c70d104352cc705d35eec746c60965
Benjamin Bentmann <unknown-email>
2011-03-19 23:23:13 +0000

    o Moved more common code into AbstractJettyStandaloneLocalConfiguration

--- core/containers/jetty/src/main/java/org/codehaus/cargo/container/jetty/Jetty7xStandaloneLocalConfiguration.java
+++ core/containers/jetty/src/main/java/org/codehaus/cargo/container/jetty/Jetty7xStandaloneLocalConfiguration.java
@@ -19,14 +19,12 @@
  */
 package org.codehaus.cargo.container.jetty;
 
-import java.io.File;
-
 import org.apache.tools.ant.types.FilterChain;
 import org.codehaus.cargo.container.InstalledLocalContainer;
-import org.codehaus.cargo.container.LocalContainer;
 import org.codehaus.cargo.container.configuration.ConfigurationCapability;
 import org.codehaus.cargo.container.jetty.internal.AbstractJettyStandaloneLocalConfiguration;
 import org.codehaus.cargo.container.jetty.internal.Jetty7xStandaloneLocalConfigurationCapability;
+import org.codehaus.cargo.container.spi.deployer.AbstractCopyingInstalledLocalDeployer;
 
 /**
  * Jetty 7.x standalone
@@ -61,54 +59,35 @@ public class Jetty7xStandaloneLocalConfiguration extends
         return capability;
     }
 
-    /**
-     * {@inheritDoc}
-     * @see org.codehaus.cargo.container.spi.configuration.AbstractLocalConfiguration#configure(LocalContainer)
-     */
     @Override
-    protected void doConfigure(LocalContainer container) throws Exception
+    protected FilterChain createJettyFilterChain()
     {
-        setupConfigurationDir();
-
         FilterChain filterChain = createFilterChain();
+
         String sessionPath = getPropertyValue(JettyPropertySet.SESSION_PATH);
         String sessionContextParam = "";
-
         if (sessionPath != null)
         {
-            sessionContextParam = "  <context-param>\n"
+            sessionContextParam =
+                "  <context-param>\n"
                     + "    <param-name>org.eclipse.jetty.servlet.SessionPath</param-name>\n"
                     + "    <param-value>" + sessionPath + "</param-value>\n"
                     + "  </context-param>\n";
         }
 
-        getAntUtils().addTokenToFilterChain(filterChain, "cargo.jetty.session.path.context-param",
-                sessionContextParam);
+        getAntUtils().addTokenToFilterChain(filterChain,
+            "cargo.jetty.session.path.context-param", sessionContextParam);
 
-        String etcDir = getFileHandler().createDirectory(getHome(), "etc");
-        getResourceUtils().copyResource(RESOURCE_PATH + container.getId()
-            + "/jetty.xml", new File(etcDir, "jetty.xml"));
-        getResourceUtils().copyResource(RESOURCE_PATH + container.getId()
-                + "/webdefault.xml", new File(etcDir, "webdefault.xml"), filterChain);
+        return filterChain;
+    }
 
-        // Create a webapps directory for automatic deployment of WARs dropped inside.
-        String appDir = getFileHandler().createDirectory(getHome(), "webapps");
-
-        // Create log directory
-        getFileHandler().createDirectory(getHome(), "logs");
-
-        // Create contexts directory for hot deployments
-        getFileHandler().createDirectory(getHome(), "contexts");
-
-        // Deploy all deployables into the webapps directory.
-        Jetty7xInstalledLocalDeployer deployer =
-            new Jetty7xInstalledLocalDeployer((InstalledLocalContainer) container);
+    @Override
+    protected AbstractCopyingInstalledLocalDeployer createDeployer(
+        InstalledLocalContainer container)
+    {
+        Jetty7xInstalledLocalDeployer deployer = new Jetty7xInstalledLocalDeployer(container);
         deployer.setShouldDeployExpandedWARs(true);
-        deployer.deploy(getDeployables());
-
-        // Deploy the CPC (Cargo Ping Component) to the webapps directory
-        getResourceUtils().copyResource(RESOURCE_PATH + "cargocpc.war",
-            new File(appDir, "cargocpc.war"));
+        return deployer;
     }
 
     /**
--- core/containers/jetty/src/main/java/org/codehaus/cargo/container/jetty/internal/AbstractJettyStandaloneLocalConfiguration.java
+++ core/containers/jetty/src/main/java/org/codehaus/cargo/container/jetty/internal/AbstractJettyStandaloneLocalConfiguration.java
@@ -19,8 +19,14 @@
  */
 package org.codehaus.cargo.container.jetty.internal;
 
+import java.io.File;
+
+import org.apache.tools.ant.types.FilterChain;
+import org.codehaus.cargo.container.InstalledLocalContainer;
+import org.codehaus.cargo.container.LocalContainer;
 import org.codehaus.cargo.container.property.GeneralPropertySet;
 import org.codehaus.cargo.container.spi.configuration.AbstractStandaloneLocalConfiguration;
+import org.codehaus.cargo.container.spi.deployer.AbstractCopyingInstalledLocalDeployer;
 
 /**
  * Base class for Jetty standalone configurations.
@@ -40,6 +46,60 @@ public abstract class AbstractJettyStandaloneLocalConfiguration extends
         super(dir);
 
         setProperty(GeneralPropertySet.RMI_PORT, "8079");
+    }
+
+    /**
+     * Creates the filter chain that should be applied while copying container configuration files
+     * to the working directory from which the container is started.
+     * 
+     * @return The filter chain, never {@code null}.
+     */
+    protected abstract FilterChain createJettyFilterChain();
+
+    /**
+     * Creates a new deployer for the specified container.
+     * 
+     * @param container The container for which to create the deployer, must not be {@code null}.
+     * @return The new deployer, never {@code null}.
+     */
+    protected abstract AbstractCopyingInstalledLocalDeployer createDeployer(
+        InstalledLocalContainer container);
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.codehaus.cargo.container.spi.configuration.AbstractLocalConfiguration#configure(LocalContainer)
+     */
+    @Override
+    protected void doConfigure(LocalContainer container) throws Exception
+    {
+        setupConfigurationDir();
+
+        FilterChain filterChain = createJettyFilterChain();
+
+        String etcDir = getFileHandler().createDirectory(getHome(), "etc");
+        getResourceUtils().copyResource(RESOURCE_PATH + container.getId() + "/jetty.xml",
+            new File(etcDir, "jetty.xml"));
+        getResourceUtils().copyResource(RESOURCE_PATH + container.getId() + "/webdefault.xml",
+            new File(etcDir, "webdefault.xml"), filterChain);
+
+        // Create a webapps directory for automatic deployment of WARs dropped inside.
+        String appDir = getFileHandler().createDirectory(getHome(), "webapps");
+
+        // Create log directory
+        getFileHandler().createDirectory(getHome(), "logs");
+
+        // Create contexts directory for hot deployments
+        getFileHandler().createDirectory(getHome(), "contexts");
+
+        // Deploy all deployables into the webapps directory.
+        AbstractCopyingInstalledLocalDeployer deployer =
+            createDeployer((InstalledLocalContainer) container);
+        deployer.deploy(getDeployables());
+
+        // Deploy the CPC (Cargo Ping Component) to the webapps directory
+        getResourceUtils().copyResource(RESOURCE_PATH + "cargocpc.war",
+            new File(appDir, "cargocpc.war"));
     }
 
 }
