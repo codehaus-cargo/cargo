3b7e0157a02401b0e4a5c31fbf814fc7bef3667d
S. Ali Tokmen <nospam@alishomepage.com>
2011-06-20 20:41:11 +0000

    Fix CARGO-998 : Add a datasource Maven2 artifact

---
+++ extensions/maven2/archetypes/webapp-with-datasource/src/it/generate-and-build/prebuild.groovy
@@ -0,0 +1,29 @@
+println "Removing project generated in earlier build"
+def directory = new File(basedir, "generate-and-build")
+
+// The complicated code below can be replaced with deleteDir() once
+// maven-invoker-plugin 1.6 is released with updated Groovy support (MINVOKER-113).
+
+if (directory.exists())
+{
+  def dirs = []
+
+  directory.eachFileRecurse
+  {
+    if (!it.isDirectory())
+    {
+      it.delete()
+    }
+    else
+    {
+      dirs << it
+    }
+  }
+
+  dirs.reverse().each
+  {
+    it.delete()
+  }
+}
+
+return true
---
+++ extensions/maven2/archetypes/webapp-with-datasource/src/main/resources/archetype-resources/src/main/java/DataSourceServlet.java
@@ -0,0 +1,55 @@
+package ${package};
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+public class DataSourceServlet extends HttpServlet
+{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        Connection c = null;
+        try
+        {
+            DataSource ds =
+                (DataSource) new InitialContext().lookup("java:comp/env/jdbc/CargoDS");
+            c = ds.getConnection();
+            PrintWriter out = response.getWriter();
+            out.print("Got connection!");
+            out.close();
+        }
+        catch (SQLException e)
+        {
+            throw new ServletException(e);
+        }
+        catch (NamingException e)
+        {
+            throw new ServletException(e);
+        }
+        finally
+        {
+            try
+            {
+                if (c != null)
+                {
+                    c.close();
+                }
+            }
+            catch (SQLException e)
+            {
+                throw new ServletException(e);
+            }
+        }
+    }
+}
