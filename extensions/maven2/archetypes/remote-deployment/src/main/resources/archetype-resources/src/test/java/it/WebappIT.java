3d0ea321776a9d8c29dff3a7f4e8c7fe5efb6e92
S. Ali Tokmen <nospam@alishomepage.com>
2011-06-21 20:50:34 +0000

    Fix CARGO-1000 : Add a remote deployment Maven2 archetype

---
+++ extensions/maven2/archetypes/remote-deployment/src/main/resources/archetype-resources/src/test/java/it/WebappIT.java
@@ -0,0 +1,27 @@
+package ${package}.it;
+
+import junit.framework.TestCase;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+public class WebappIT extends TestCase
+{
+    private String baseUrl;
+    
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        String host = System.getProperty("hostname");
+        String port = System.getProperty("servlet.port");
+        this.baseUrl = "http://" + host + ":" + port + "/${artifactId}";
+    }
+
+    public void testCallIndexPage() throws Exception
+    {
+        URL url = new URL(this.baseUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        assertEquals(200, connection.getResponseCode());
+    }
+}
