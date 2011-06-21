419247d7c9c35fe65e3424c4f9df7fda081809bf
S. Ali Tokmen <nospam@alishomepage.com>
2011-06-21 17:30:02 +0000

    Remove the @Override annotation else build fails with Maven2, since the default Java version in Maven2 is 1.3

--- extensions/maven2/archetypes/webapp-with-datasource/src/main/resources/archetype-resources/src/main/java/DataSourceServlet.java
+++ extensions/maven2/archetypes/webapp-with-datasource/src/main/resources/archetype-resources/src/main/java/DataSourceServlet.java
@@ -15,7 +15,6 @@ import javax.sql.DataSource;
 
 public class DataSourceServlet extends HttpServlet
 {
-    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
