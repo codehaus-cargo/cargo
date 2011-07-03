3080120dcc4c6b037b097066974d01e55b8abd59
S. Ali Tokmen <nospam@alishomepage.com>
2011-07-03 21:15:45 +0000

    Upgrade to Jetty 7.4.3.v20110701

--- extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/jetty/JettyArtifactResolver.java
+++ extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/jetty/JettyArtifactResolver.java
@@ -165,25 +165,25 @@ public class JettyArtifactResolver
         jetty7xDependencies.add(new Dependency("javax.servlet", "servlet-api", "2.5"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jdt.core.compiler", "ecj", "3.5.1"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-continuation",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-http",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-io",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-jsp-2.1",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-util",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-security",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-server",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-servlet",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-webapp",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-xml",
-            "7.4.2.v20110526"));
+            "7.4.3.v20110701"));
         jetty7xDependencies.add(new Dependency("org.mortbay.jetty", "jsp-2.1-glassfish",
             "2.1.v20100127"));
         jetty7xDependencies.add(new Dependency("org.mortbay.jetty", "jsp-api-2.1-glassfish",
