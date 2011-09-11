fb86105408b7febb11f97bd7feb6900a364bad52
Benjamin Bentmann <unknown-email>
2011-09-11 12:18:41 +0000

    o Renamed src directory for ITs to shorten pathnames which otherwise exceed the magical 256 char limit on Windows and cause bogus test failures

--- extensions/maven2/archetypes/webapp-with-datasource/src/it/it/prebuild.groovy
+++ extensions/maven2/archetypes/webapp-with-datasource/src/it/it/prebuild.groovy
@@ -1,5 +1,5 @@
 println "Removing project generated in earlier build"
-def directory = new File(basedir, "generate-and-build")
+def directory = new File(basedir, "it")
 
 // The complicated code below can be replaced with deleteDir() once
 // maven-invoker-plugin 1.6 is released with updated Groovy support (MINVOKER-113).
