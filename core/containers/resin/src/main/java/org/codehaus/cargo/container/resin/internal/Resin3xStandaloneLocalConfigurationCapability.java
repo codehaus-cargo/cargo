60d9bfca031a5bfd72ebabf2b83ee6ab12a20239
S. Ali Tokmen <nospam@alishomepage.com>
2012-01-14 16:05:31 +0000

    Fix CARGO-318: Hardcoded Resin management port is 7777

---
+++ core/containers/resin/src/main/java/org/codehaus/cargo/container/resin/internal/Resin3xStandaloneLocalConfigurationCapability.java
@@ -0,0 +1,44 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus CARGO, copyright 2004-2011 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.container.resin.internal;
+
+import org.codehaus.cargo.container.resin.ResinPropertySet;
+
+/**
+ * Capabilities of the Resin's
+ * {@link org.codehaus.cargo.container.resin.Resin3xStandaloneLocalConfiguration}
+ * configuration.
+ * 
+ * @version $Id$
+ */
+public class Resin3xStandaloneLocalConfigurationCapability
+    extends Resin2xStandaloneLocalConfigurationCapability
+{
+
+    /**
+     * Initialize the configuration-specific supports Map.
+     */
+    public Resin3xStandaloneLocalConfigurationCapability()
+    {
+        super();
+
+        this.supportsMap.put(ResinPropertySet.SOCKETWAIT_PORT, Boolean.TRUE);
+    }
+}
