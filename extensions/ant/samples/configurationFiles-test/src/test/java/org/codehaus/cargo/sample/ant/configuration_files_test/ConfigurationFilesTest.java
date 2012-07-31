cf7184c441ee78e0e6a22f974b573ffa58057b8f
S. Ali Tokmen <nospam@alishomepage.com>
2012-07-31 17:03:42 +0000

    Added a new ANT sample for configuration files

---
+++ extensions/ant/samples/configurationFiles-test/src/test/java/org/codehaus/cargo/sample/ant/configuration_files_test/ConfigurationFilesTest.java
@@ -0,0 +1,91 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus CARGO, copyright 2004-2011 Vincent Massol.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.ant.configuration_files_test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+public class ConfigurationFilesTest extends TestCase
+{
+
+    public void testConfigFileDirectory() throws Exception
+    {
+        Properties properties = loadProperties("test-configFile-directory/test-subfolder");
+        assertEquals("12345", properties.getProperty("cargo.servlet.port"));
+    }
+
+    public void testConfigFileFile() throws Exception
+    {
+        Properties properties = loadProperties("test-configFile-file");
+        assertEquals("12345", properties.getProperty("cargo.servlet.port"));
+    }
+
+    public void testFileDirectory() throws Exception
+    {
+        Properties properties = loadProperties("test-file-directory/test-subfolder");
+        assertEquals("@cargo.servlet.port@", properties.getProperty("cargo.servlet.port"));
+    }
+
+    public void testFileFile() throws Exception
+    {
+        Properties properties = loadProperties("test-file-file");
+        assertEquals("@cargo.servlet.port@", properties.getProperty("cargo.servlet.port"));
+    }
+
+    public void testFileDirectoryWithConfigOption() throws Exception
+    {
+        Properties properties = loadProperties("test-file-configfile-directory/test-subfolder");
+        assertEquals("12345", properties.getProperty("cargo.servlet.port"));
+    }
+
+    public void testFileFileWithConfigOption() throws Exception
+    {
+        Properties properties = loadProperties("test-file-configfile-file");
+        assertEquals("12345", properties.getProperty("cargo.servlet.port"));
+    }
+
+    private Properties loadProperties(String subDirectory) throws Exception
+    {
+        File catalinaBase = new File("target/catalina-base");
+        assertTrue(catalinaBase + " is not a directory", catalinaBase.isDirectory());
+
+        File propertiesFile = new File(catalinaBase, subDirectory + "/test.properties");
+        assertTrue(propertiesFile + " is not a file", propertiesFile.isFile());
+
+        Properties properties = new Properties();
+        FileInputStream fis = new FileInputStream(propertiesFile);
+        try
+        {
+            properties.load(fis);
+        }
+        finally
+        {
+            fis.close();
+            fis = null;
+            System.gc();
+        }
+
+        return properties;
+    }
+
+}
