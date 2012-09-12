149b8c0acfbb6a11d4747196ec092e8d9f999038
S. Ali Tokmen <nospam@alishomepage.com>
2012-09-12 19:48:27 +0000

    Upgrade to Jetty 7.6.7.v20120910 and 8.1.7.v20120910

--- extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/AbstractCargoMojo.java
+++ extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/AbstractCargoMojo.java
@@ -726,7 +726,7 @@ public abstract class AbstractCargoMojo extends AbstractCommonMojo
             ArtifactInstaller artifactInstaller = new ArtifactInstaller();
             artifactInstaller.setGroupId("org.eclipse.jetty");
             artifactInstaller.setArtifactId("jetty-distribution");
-            artifactInstaller.setVersion("7.6.6.v20120903");
+            artifactInstaller.setVersion("7.6.7.v20120910");
             getContainerElement().setArtifactInstaller(artifactInstaller);
 
             getLog().info("No container defined, using a default ["
