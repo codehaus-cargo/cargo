2116729a65ff4ee05b4373f1a8c3af9f56dcb4f6
S. Ali Tokmen <nospam@alishomepage.com>
2013-11-19 05:41:29 +0000

    Upgrade to Jetty 9.1.0.v20131115

--- extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/jetty/JettyArtifactResolver.java
+++ extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/jetty/JettyArtifactResolver.java
@@ -231,29 +231,29 @@ public class JettyArtifactResolver
 
         List<Dependency> jetty9xDependencies = new ArrayList<Dependency>();
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-annotations",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-continuation",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-http",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-io",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-jndi",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-plus",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-util",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-security",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-server",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-servlet",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-webapp",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("org.eclipse.jetty", "jetty-xml",
-            "9.1.0.RC2"));
+            "9.1.0.v20131115"));
         jetty9xDependencies.add(new Dependency("javax.servlet", "javax.servlet-api", "3.1.0"));
         // annotations
         jetty9xDependencies.add(new Dependency("org.ow2.asm", "asm", "4.1"));
