4735ddf0ea41b030f8089383ce3d91ac07a56fef
S. Ali Tokmen <nospam@alishomepage.com>
2014-05-07 18:39:28 +0000

    * Upgrade to Jetty 7.6.15.v20140411 * Upgrade to Jetty 8.1.15.v20140411 * Upgrade to Jetty 9.1.5.v20140505 * Fix CARGO-1271: Starting Tomcat 7 with Cargo logs warning on emptySessionPath * Fix CARGO-1272: Starting Tomcat generates warnings on not existing folders in classloader

--- extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/AbstractCargoMojo.java
+++ extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/AbstractCargoMojo.java
@@ -768,7 +768,7 @@ public abstract class AbstractCargoMojo extends AbstractCommonMojo
             ArtifactInstaller artifactInstaller = new ArtifactInstaller();
             artifactInstaller.setGroupId("org.eclipse.jetty");
             artifactInstaller.setArtifactId("jetty-distribution");
-            artifactInstaller.setVersion("7.6.14.v20131031");
+            artifactInstaller.setVersion("7.6.15.v20140411");
             getContainerElement().setArtifactInstaller(artifactInstaller);
 
             getLog().info("No container defined, using a default ["
