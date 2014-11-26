01a75c270c185d1e7293e7e3f8a6210cd1af6d38
S. Ali Tokmen <nospam@alishomepage.com>
2014-11-26 21:36:18 +0000

    Add cargo.samples.exclude

--- core/samples/java/src/test/java/org/codehaus/cargo/sample/java/tomcat/TomcatTLSTest.java
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/tomcat/TomcatTLSTest.java
@@ -58,7 +58,6 @@ public class TomcatTLSTest extends AbstractCargoTestCase
     public TomcatTLSTest(String testName, EnvironmentTestData testData) throws Exception
     {
         super(testName, testData);
-        System.setProperty("javax.net.debug", "all");
     }
 
     /**
