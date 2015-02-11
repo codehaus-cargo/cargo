e17987a2320cd89c2aff6ccc407a0c44011386c3
S. Ali Tokmen <nospam@alishomepage.com>
2015-02-11 07:18:43 +0000

    Finish implementing CARGO-1300 (Tomcat 6.x, 7.x and 8.x embedded support) with the addition of the embedded Tomcat 8.x container

--- extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/util/EmbeddedContainerArtifactResolver.java
+++ extensions/maven2/plugin/src/main/java/org/codehaus/cargo/maven2/util/EmbeddedContainerArtifactResolver.java
@@ -317,6 +317,27 @@ public class EmbeddedContainerArtifactResolver
         tomcat7xDependencies.add(new Dependency("org.eclipse.jdt.core.compiler", "ecj",
             "4.4"));
         this.containerDependencies.put("tomcat7x", tomcat7xDependencies);
+
+        List<Dependency> tomcat8xDependencies = new ArrayList<Dependency>();
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-servlet-api",
+            "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-annotations-api",
+            "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-el-api", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-jsp-api", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-juli", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-api", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-util", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-util-scan",
+            "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-catalina", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-coyote", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-jasper", "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.apache.tomcat", "tomcat-jasper-el",
+            "8.0.18"));
+        tomcat8xDependencies.add(new Dependency("org.eclipse.jdt.core.compiler", "ecj",
+            "4.4"));
+        this.containerDependencies.put("tomcat8x", tomcat8xDependencies);
     }
 
     /**
