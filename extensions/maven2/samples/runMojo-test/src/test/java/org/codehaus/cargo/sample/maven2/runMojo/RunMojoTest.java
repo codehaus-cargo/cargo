74e82f8d9bddea14217a36ff5cbe1c5a041e2ab9
alitokmen <nospam@alishomepage.com>
2016-11-05 13:49:48 +0100

    Fix CARGO-1413 / Enable the cargo:run mojo test

--- extensions/maven2/samples/runMojo-test/src/test/java/org/codehaus/cargo/sample/maven2/runMojo/RunMojoTest.java
+++ extensions/maven2/samples/runMojo-test/src/test/java/org/codehaus/cargo/sample/maven2/runMojo/RunMojoTest.java
@@ -19,9 +19,15 @@
  */
 package org.codehaus.cargo.sample.maven2.runMojo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.net.URL;
 
 import junit.framework.TestCase;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.cli.MavenCli;
 
 import org.codehaus.cargo.sample.java.PingUtils;
 import org.codehaus.cargo.util.log.Logger;
@@ -34,9 +40,72 @@ public class RunMojoTest extends TestCase
 {
 
     /**
+     * Whether the initialization is complete.
+     */
+    private static boolean initialized = false;
+
+    /**
      * Logger.
      */
     private Logger logger = new SimpleLogger();
+
+    /**
+     * Project directory.
+     */
+    private File projectDirectory;
+
+    /**
+     * Project output file.
+     */
+    private File output;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        File target = new File(System.getProperty("target"));
+        this.projectDirectory = new File(target, "classes").getAbsoluteFile();
+
+        this.output = new File(target, "output.log");
+
+        synchronized (RunMojoTest.class)
+        {
+            if (!RunMojoTest.initialized)
+            {
+                final PrintStream outputStream = new PrintStream(output);
+
+                String portOption = "-Dcargo.samples.servlet.port="
+                    + System.getProperty("http.port");
+                final String[] options =
+                    new String[] {portOption, "-o", "-X", "clean", "cargo:run"};
+
+                new Thread(new Runnable()
+                {
+                    public void run()
+                    {
+                        MavenCli maven2 = new MavenCli();
+                        maven2.doMain(
+                            options , projectDirectory.getPath(), outputStream, outputStream);
+                    }
+                }).start();
+
+                RunMojoTest.initialized = true;
+            }
+        }
+    }
+
+    /**
+     * Test the Maven2/Maven3 <code>cargo:run</code> mojo.
+     * @throws Exception If anything fails.
+     */
+    public void testRunMojo() throws Exception
+    {
+        waitForRunMojoStart();
+    }
 
     /**
      * Test the CARGO ping component.
@@ -44,6 +113,8 @@ public class RunMojoTest extends TestCase
      */
     public void testCargo() throws Exception
     {
+        waitForRunMojoStart();
+
         final URL url = new URL("http://localhost:" + System.getProperty("http.port")
             + "/cargocpc/");
         final String expected = "Cargo Ping Component";
@@ -57,11 +128,50 @@ public class RunMojoTest extends TestCase
      */
     public void testSimpleWarJsp() throws Exception
     {
+        waitForRunMojoStart();
+
         final URL url = new URL("http://localhost:" + System.getProperty("http.port")
             + "/simple-war/index.jsp");
         final String expected = "Sample page for testing";
 
         PingUtils.assertPingTrue(url.getPath() + " not started", expected, url, logger);
+    }
+
+    /**
+     * Wait for the <code>cargo:run</code> mojo to start.
+     * @throws Exception If anything fails.
+     */
+    private void waitForRunMojoStart() throws Exception
+    {
+        String outputString = null;
+        long timeout = 90 * 1000 + System.currentTimeMillis();
+        while (System.currentTimeMillis() < timeout)
+        {
+            try
+            {
+                outputString = FileUtils.readFileToString(output);
+            }
+            catch (FileNotFoundException e)
+            {
+                outputString = e.toString();
+            }
+
+            if (outputString.contains("Press Ctrl-C to stop the container..."))
+            {
+                return;
+            }
+            else if (outputString.contains("BUILD FAILURE"))
+            {
+                fail("There has been a BUILD FAILURE. Please check file " + output);
+                return;
+            }
+
+            Thread.sleep(1000);
+            System.gc();
+        }
+
+        fail("The file " + output + " did not have the Ctrl-C message after 90 seconds. "
+            + "Current content: \n\n" + outputString);
     }
 
 }
