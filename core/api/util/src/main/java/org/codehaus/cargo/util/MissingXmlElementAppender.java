8cd9fb335a95378a1d6d1fe7780d412448bc0651
Christian Mouttet <cmouttet@gmail.com>
2018-09-01 18:16:26 +0200

    add MissingXmlElementAppender

---
+++ core/api/util/src/main/java/org/codehaus/cargo/util/MissingXmlElementAppender.java
@@ -0,0 +1,159 @@
+package org.codehaus.cargo.util;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class detects missing nodes of a DOM document based on an XPath expression and
+ * appends the missing branch.
+ */
+class MissingXmlElementAppender
+{
+    /**
+     * Document that may have missing nodes
+     */
+    private final Document document;
+
+    /**
+     * XPath, split up into path elements
+     */
+    private final LinkedList<String> expressionNodes = new LinkedList<String>();
+
+    /**
+     *
+     * @param document Document that may have missing nodes
+     * @param xPathString XPath that needs to be present in the document
+     */
+    MissingXmlElementAppender(Document document, String xPathString)
+    {
+        this.document = document;
+
+        String regex = "/(?=(?:[^']*'[^']*')*[^']*$)";
+        this.expressionNodes.addAll(Arrays.asList(xPathString.split(regex)));
+    }
+
+    /**
+     * Getter
+     * @return documnet given via the constructor
+     */
+    Document getDocument()
+    {
+        return document;
+    }
+
+    /**
+     * Appends the missing nodes.
+     *
+     * @return the common node of document and XPath
+     * @throws XPathExpressionException if anything goes wrong
+     */
+    Node append() throws XPathExpressionException
+    {
+        LinkedList<String> missingNodes = new LinkedList<String>();
+        Node evaluate = getMostCommonNode(missingNodes);
+
+        if ((evaluate != null) && (!missingNodes.isEmpty()))
+        {
+            evaluate = appendMissingElements(missingNodes, evaluate);
+        }
+
+        return evaluate;
+    }
+
+    /**
+     * Find the common node of the given document and the XPath.
+     * Based on this node the hierarchy will be appended.
+     *
+     * @param missingNodes as a side effect the node elements are collected
+     * @return common node of the given document and the XPath
+     * @throws XPathExpressionException if anything goes wrong
+     */
+    private Node getMostCommonNode(LinkedList<String> missingNodes) throws XPathExpressionException
+    {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        Object evaluate;
+        do
+        {
+            String expression = toXPathString(expressionNodes);
+            XPathExpression xPathExpression = xPath.compile(expression);
+            evaluate = xPathExpression.evaluate(document, XPathConstants.NODE);
+
+            if (evaluate == null)
+            {
+                String lastNode = expressionNodes.removeLast();
+                missingNodes.addFirst(lastNode);
+            }
+        }
+        while ((evaluate == null) && !expressionNodes.isEmpty());
+
+        if (evaluate != null)
+        {
+            return (Node) evaluate;
+        }
+        return null;
+    }
+
+    /**
+     * Assemble a XPath string based on a list of elements
+     * @param expressionNodes split up XPath elements
+     * @return XPath string
+     */
+    private String toXPathString(List<String> expressionNodes)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Iterator<String> iterator = expressionNodes.iterator(); iterator.hasNext();)
+        {
+            String expressionNode = iterator.next();
+            stringBuilder.append(expressionNode);
+            if (iterator.hasNext())
+            {
+                stringBuilder.append("/");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Adds the elements of missingNodes as element hierarchy to the node
+     * @param missingNodes collected list of the missing elements
+     * @param node target of the additions
+     * @return the last node added to the document
+     */
+    private Node appendMissingElements(List<String> missingNodes, Node node)
+    {
+        Pattern elementOnlyPattern = Pattern.compile("([^\\[]+)\\[.+");
+        Node result = node;
+
+        for (String missingNode : missingNodes)
+        {
+            if (!missingNode.startsWith("@"))
+            {
+                Matcher matcher = elementOnlyPattern.matcher(missingNode);
+                if (matcher.matches())
+                {
+                    missingNode = matcher.group(1);
+                }
+
+                Element newElement = document.createElement(missingNode);
+                result.appendChild(newElement);
+                result = newElement;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ core/api/util/src/test/java/org/codehaus/cargo/util/MissingXmlElementAppenderTest.java
@@ -0,0 +1,129 @@
+package org.codehaus.cargo.util;
+
+import junit.framework.TestCase;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
+
+/**
+ * Test for MissingXmlElementAppender.
+ */
+public class MissingXmlElementAppenderTest extends TestCase
+{
+    /**
+     * Nothing will be appended, document is already as expected.
+     * @throws Exception if anything goes wrong
+     */
+    public void testAppendNotNeeded() throws Exception
+    {
+        String xml = "<Root/>";
+        String xPath = "/Root";
+
+        MissingXmlElementAppender missingXmlElementAppender =
+                new MissingXmlElementAppender(parse(xml), xPath);
+        missingXmlElementAppender.append();
+        assertEquals("<Root/>", toString(missingXmlElementAppender.getDocument()));
+    }
+
+    /**
+     * Assert that one hierarchy level will be added.
+     * @throws Exception if anything goes wrong
+     */
+    public void testAppendBranch() throws Exception
+    {
+        String xml = "<Root/>";
+        String xPath = "/Root/Branch";
+
+        MissingXmlElementAppender missingXmlElementAppender =
+                new MissingXmlElementAppender(parse(xml), xPath);
+        missingXmlElementAppender.append();
+        assertEquals("<Root><Branch/></Root>", toString(missingXmlElementAppender.getDocument()));
+    }
+
+    /**
+     * Assert that more hierarchy levels will be added
+     * @throws Exception if anything goes wrong
+     */
+    public void testAppendLeave() throws Exception
+    {
+        String xml = "<Root/>";
+        String xPath = "/Root/Branch/Leave";
+
+        MissingXmlElementAppender missingXmlElementAppender =
+                new MissingXmlElementAppender(parse(xml), xPath);
+        missingXmlElementAppender.append();
+        assertEquals("<Root><Branch><Leave/></Branch></Root>",
+                toString(missingXmlElementAppender.getDocument()));
+    }
+
+    /**
+     * Assert that an element with attribute is correctly found
+     * @throws Exception if anything goes wrong
+     */
+    public void testAppendLeaveForBranchWithAttribute() throws Exception
+    {
+        String xml = "<Root><Branch key='value'/></Root>";
+        String xPath = "/Root/Branch[@key='value']/Leave";
+
+        MissingXmlElementAppender missingXmlElementAppender =
+                new MissingXmlElementAppender(parse(xml), xPath);
+        missingXmlElementAppender.append();
+        assertEquals("<Root><Branch key=\"value\"><Leave/></Branch></Root>",
+                toString(missingXmlElementAppender.getDocument()));
+    }
+
+    /**
+     * Assert that an element with a child is correctly found
+     * @throws Exception if anything goes wrong
+     */
+    public void testAppendLeaveForBranchWithChild() throws Exception
+    {
+        String xml = "<Root><Branch><Child/></Branch></Root>";
+        String xPath = "/Root/Branch[Child]/Leave";
+
+        MissingXmlElementAppender missingXmlElementAppender =
+                new MissingXmlElementAppender(parse(xml), xPath);
+        missingXmlElementAppender.append();
+        assertEquals("<Root><Branch><Child/><Leave/></Branch></Root>",
+                toString(missingXmlElementAppender.getDocument()));
+    }
+
+    /**
+     * Parse XML string
+     *
+     * @param xml string representation
+     * @return DOM document
+     * @throws Exception if anything goes wrong
+     */
+    private Document parse(String xml) throws Exception
+    {
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        return documentBuilder.parse(new ByteArrayInputStream(xml.getBytes()));
+    }
+
+    /**
+     * Convert the DOM document to its String representation.
+     *
+     * @param document DOM that is to be converted
+     * @return String representation of the document
+     * @throws TransformerException if anything goes wrong
+     */
+    private String toString(Document document) throws TransformerException
+    {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(document), new StreamResult(writer));
+        return writer.getBuffer().toString().replaceAll("\n|\r", "");
+    }
+}
