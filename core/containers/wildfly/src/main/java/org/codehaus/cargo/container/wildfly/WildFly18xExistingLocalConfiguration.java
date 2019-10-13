ad2a95305b31e46670adfa7a8cd43073c0eced1c
alitokmen <nospam@alishomepage.com>
2019-10-13 12:43:44 +0200

    CARGO-1500: Add support for WildFly 18.x

---
+++ core/containers/wildfly/src/main/java/org/codehaus/cargo/container/wildfly/WildFly18xExistingLocalConfiguration.java
@@ -0,0 +1,45 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus CARGO, copyright 2004-2011 Vincent Massol, 2012-2019 Ali Tokmen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.container.wildfly;
+
+/**
+ * WildFly 18.x existing {@link org.codehaus.cargo.container.configuration.Configuration}
+ * implementation.
+ */
+public class WildFly18xExistingLocalConfiguration extends WildFly17xExistingLocalConfiguration
+{
+    /**
+     * {@inheritDoc}
+     * @see WildFly17xExistingLocalConfiguration#WildFly17xExistingLocalConfiguration(String)
+     */
+    public WildFly18xExistingLocalConfiguration(String dir)
+    {
+        super(dir);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString()
+    {
+        return "WildFly 18.x Existing Configuration";
+    }
+}
