2201f8f6afaf39b20d5b36f070ad6d1bf59c15fc
alitokmen <nospam@alishomepage.com>
2020-06-10 22:02:38 +0200

    WildFly 20.x cannot deploy JMS resources

--- core/samples/java/src/test/java/org/codehaus/cargo/sample/java/JmsQueueResourceOnStandaloneConfigurationTest.java
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/JmsQueueResourceOnStandaloneConfigurationTest.java
@@ -78,7 +78,8 @@ public class JmsQueueResourceOnStandaloneConfigurationTest extends
             new CargoTestSuite(
                 "Tests that run on local containers supporting Resource and WAR deployments");
 
-        // JBoss 7.5.x, JRun, Resin and Tomcat containers cannot deploy JMS queue resources
+        // JBoss 7.5.x, JRun, Resin, Tomcat as well as WildFly 20.x containers cannot deploy JMS
+        // queue resources
         Set<String> excludedContainerIds = new TreeSet<String>();
         excludedContainerIds.add("jboss75x");
         excludedContainerIds.add("resin2x");
@@ -92,6 +93,7 @@ public class JmsQueueResourceOnStandaloneConfigurationTest extends
         excludedContainerIds.add("tomcat8x");
         excludedContainerIds.add("tomcat9x");
         excludedContainerIds.add("tomcat10x");
+        excludedContainerIds.add("wildfly20x");
 
         suite.addTestSuite(JmsQueueResourceOnStandaloneConfigurationTest.class,
             new Validator[] {
--- core/samples/java/src/test/java/org/codehaus/cargo/sample/java/JmsTopicResourceOnStandaloneConfigurationTest.java
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/JmsTopicResourceOnStandaloneConfigurationTest.java
@@ -78,7 +78,7 @@ public class JmsTopicResourceOnStandaloneConfigurationTest extends
             new CargoTestSuite(
                 "Tests that run on local containers supporting Resource and WAR deployments");
 
-        // JBoss 7.5.x, Resin, Tomcat and WebLogic 12.1.x, 12.2.x and 14.x
+        // JBoss 7.5.x, Resin, Tomcat, WebLogic 12.1.x, 12.2.x and 14.x as well as WildFly 20.x
         // containers cannot deploy JMS topic resources
         Set<String> excludedContainerIds = new TreeSet<String>();
         excludedContainerIds.add("jboss75x");
@@ -96,6 +96,7 @@ public class JmsTopicResourceOnStandaloneConfigurationTest extends
         excludedContainerIds.add("weblogic121x");
         excludedContainerIds.add("weblogic122x");
         excludedContainerIds.add("weblogic14x");
+        excludedContainerIds.add("wildfly20x");
 
         suite.addTestSuite(JmsTopicResourceOnStandaloneConfigurationTest.class,
             new Validator[] {
