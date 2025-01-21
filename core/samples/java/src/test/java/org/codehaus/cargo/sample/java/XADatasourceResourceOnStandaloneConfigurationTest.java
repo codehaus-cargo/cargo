ecec6b9eb617725c786850e8522707401735586b
S. Ali Tokmen <nospam@alishomepage.com>
2025-01-21 21:38:11 +0100

    Add missing HasXASupportValidator

--- core/samples/java/src/test/java/org/codehaus/cargo/sample/java/XADatasourceResourceOnStandaloneConfigurationTest.java
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/XADatasourceResourceOnStandaloneConfigurationTest.java
@@ -28,8 +28,8 @@ import org.codehaus.cargo.container.configuration.ConfigurationType;
 import org.codehaus.cargo.container.configuration.entry.ConfigurationFixtureFactory;
 import org.codehaus.cargo.container.configuration.entry.ResourceFixture;
 import org.codehaus.cargo.container.property.ResourcePropertySet;
-import org.codehaus.cargo.sample.java.validator.HasDataSourceSupportValidator;
 import org.codehaus.cargo.sample.java.validator.HasResourceSupportValidator;
+import org.codehaus.cargo.sample.java.validator.HasXASupportValidator;
 
 /**
  * Test for XA datasource deployed as resource resource capabilities.
@@ -44,7 +44,7 @@ public class XADatasourceResourceOnStandaloneConfigurationTest extends
     public XADatasourceResourceOnStandaloneConfigurationTest()
     {
         new HasResourceSupportValidator(ConfigurationType.STANDALONE);
-        new HasDataSourceSupportValidator(ConfigurationType.STANDALONE);
+        new HasXASupportValidator(ConfigurationType.STANDALONE);
     }
 
     /**
