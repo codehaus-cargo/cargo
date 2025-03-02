40a1fb5293df562ca0d04fb49eaf87b6c6c544a0
alitokmen <nospam@alishomepage.com>
2025-03-02 21:27:06 +0100

    Add Jetty 12.x EE profile tests

---
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/jetty/JettyEmbeddedEE10ProfileTest.java
@@ -0,0 +1,53 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus Cargo, copyright 2004-2011 Vincent Massol, 2012-2025 Ali Tokmen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.java.jetty;
+
+import org.codehaus.cargo.container.configuration.LocalConfiguration;
+import org.codehaus.cargo.container.jetty.JettyPropertySet;
+import org.codehaus.cargo.sample.java.CargoTestCase;
+
+/**
+ * Jetty Embedded EE 10 profile test.
+ */
+public class JettyEmbeddedEE10ProfileTest extends AbstractJettyEmbeddedEEProfileTest
+{
+    /**
+     * Add the required validators and initializes the Jetty Embedded specific embedded container
+     * classpath resolver.
+     * @see AbstractJettyEmbeddedEEProfileTest#AbstractJettyEmbeddedEEProfileTest(String)
+     * @see #addValidator(org.codehaus.cargo.sample.java.validator.Validator)
+     */
+    public JettyEmbeddedEE10ProfileTest()
+    {
+        super("ee10");
+    }
+
+    /**
+     * Test Jetty Embedded with EE 10 profile.
+     * @throws Exception If anything goes wrong.
+     */
+    @CargoTestCase
+    public void testEE10Profile() throws Exception
+    {
+        LocalConfiguration configuration = getLocalContainer().getConfiguration();
+        configuration.setProperty(JettyPropertySet.DEPLOYER_EE_VERSION, "ee10");
+        testWar("simple");
+    }
+}
---
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/jetty/JettyEmbeddedEE8ProfileTest.java
@@ -0,0 +1,70 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus Cargo, copyright 2004-2011 Vincent Massol, 2012-2025 Ali Tokmen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.java.jetty;
+
+import java.net.URL;
+
+import org.codehaus.cargo.container.configuration.LocalConfiguration;
+import org.codehaus.cargo.container.deployable.DeployableType;
+import org.codehaus.cargo.container.deployable.WAR;
+import org.codehaus.cargo.container.jetty.JettyPropertySet;
+import org.codehaus.cargo.sample.java.CargoTestCase;
+import org.codehaus.cargo.sample.java.PingUtils;
+
+/**
+ * Jetty Embedded EE 8 profile test.
+ */
+public abstract class JettyEmbeddedEE8ProfileTest extends AbstractJettyEmbeddedEEProfileTest
+{
+    /**
+     * Add the required validators and initializes the Jetty Embedded specific embedded container
+     * classpath resolver.
+     * @see AbstractJettyEmbeddedEEProfileTest#AbstractJettyEmbeddedEEProfileTest(String)
+     * @see #addValidator(org.codehaus.cargo.sample.java.validator.Validator)
+     */
+    public JettyEmbeddedEE8ProfileTest()
+    {
+        super("ee8");
+    }
+
+    /**
+     * Test Jetty Embedded with EE 8 profile.
+     * @throws Exception If anything goes wrong.
+     */
+    @CargoTestCase
+    public void testEE8Profile() throws Exception
+    {
+        LocalConfiguration configuration = getLocalContainer().getConfiguration();
+        configuration.setProperty(JettyPropertySet.DEPLOYER_EE_VERSION, "ee8");
+
+        String simpleWar = getTestData().getTestDataFileFor("simple-war");
+        WAR war = (WAR) this.createDeployable(
+            simpleWar.replace("/deployables-jakarta-ee/", "/deployables/"), DeployableType.WAR);
+        configuration.addDeployable(war);
+
+        URL warPingURL =
+            new URL("http://localhost:" + getTestData().port + "/simple-war/index.jsp");
+
+        getLocalContainer().start();
+        PingUtils.assertPingTrue("simple war not started", warPingURL, getLogger());
+        getLocalContainer().stop();
+        PingUtils.assertPingFalse("simple war not stopped", warPingURL, getLogger());
+    }
+}
