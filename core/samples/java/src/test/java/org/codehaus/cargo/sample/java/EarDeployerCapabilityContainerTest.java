51e3d6df56e6ce111ebcaff3f3dabbe9c4b7d136
alitokmen <nospam@alishomepage.com>
2025-05-31 14:41:32 +0200

    Fixed CARGO-1641 (Deployer's undeploy methods ignores the EAR name) and added testcases so this doesn't reoccur again

---
+++ core/samples/java/src/test/java/org/codehaus/cargo/sample/java/EarDeployerCapabilityContainerTest.java
@@ -0,0 +1,77 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus Cargo, copyright 2004-2011 Vincent Massol, 2012-2025 Ali Tokmen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.sample.java;
+
+import java.net.URL;
+
+import org.codehaus.cargo.container.deployable.Deployable;
+import org.codehaus.cargo.container.deployable.DeployableType;
+import org.codehaus.cargo.container.deployable.EAR;
+import org.codehaus.cargo.container.deployer.DeployableMonitor;
+import org.codehaus.cargo.container.deployer.Deployer;
+import org.codehaus.cargo.container.deployer.URLDeployableMonitor;
+import org.codehaus.cargo.sample.java.validator.HasLocalDeployerValidator;
+
+/**
+ * Validates local hot deployment of EAR archives
+ */
+public class EarDeployerCapabilityContainerTest extends AbstractStandaloneLocalContainerTestCase
+{
+    /**
+     * Add the required validators.
+     * @see #addValidator(org.codehaus.cargo.sample.java.validator.Validator)
+     */
+    public EarDeployerCapabilityContainerTest()
+    {
+        this.addValidator(new HasEarSupportValidator());
+        this.addValidator(new HasLocalDeployerValidator());
+    }
+
+    /**
+     * Test EAR hot deployment.
+     * @throws Exception If anything goes wrong.
+     */
+    @CargoTestCase
+    public void testEarHotDeployment() throws Exception
+    {
+        Deployable ear = this.createDeployableFromTestdataFile("simple-ear", DeployableType.EAR);
+        ((EAR) ear).setName("ear-test-CARGO-1641");
+
+        URL earPingURL =
+            new URL("http://localhost:" + getTestData().port + "/simpleweb/index.jsp");
+
+        getLocalContainer().start();
+        PingUtils.assertPingFalse(
+            "simple ear should not be present at this point", earPingURL, getLogger());
+
+        Deployer deployer = createDeployer(getContainer());
+        DeployableMonitor deployableMonitor = new URLDeployableMonitor(earPingURL);
+        deployableMonitor.setLogger(this.getLogger());
+        deployer.deploy(ear, deployableMonitor);
+        PingUtils.assertPingTrue(
+            "simple ear should have been deployed at this point", earPingURL, getLogger());
+
+        deployer.undeploy(ear, deployableMonitor);
+        PingUtils.assertPingFalse(
+            "simple ear should have been undeployed at this point", earPingURL, getLogger());
+
+        getLocalContainer().stop();
+    }
+}
