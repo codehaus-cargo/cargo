60c1fd30182f82da0b59e7cbe0e608ffd525736e
alitokmen <nospam@alishomepage.com>
2026-01-02 18:01:50 +0100

    Update copyright year to 2026 :)

--- core/containers/wildfly-swarm/src/main/java/org/codehaus/cargo/container/wildfly/swarm/internal/WildFlySwarmStandaloneLocalConfigurationCapability.java
+++ core/containers/wildfly-swarm/src/main/java/org/codehaus/cargo/container/wildfly/swarm/internal/WildFlySwarmStandaloneLocalConfigurationCapability.java
@@ -1,7 +1,7 @@
 /*
  * ========================================================================
  *
- *  Codehaus Cargo, copyright 2004-2011 Vincent Massol, 2012-2025 Ali Tokmen.
+ *  Codehaus Cargo, copyright 2004-2011 Vincent Massol, 2012-2026 Ali Tokmen.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
