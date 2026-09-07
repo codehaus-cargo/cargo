1c5b9b1f1d2f2307b0713a99144fba14653ee74a
S. Ali Tokmen <nospam@alishomepage.com>
2026-09-07 10:15:31 +0200

    Use Assertions. instead or Assert.

--- core/api/util/src/test/java/org/codehaus/cargo/util/DefaultFileHandlerTest.java
+++ core/api/util/src/test/java/org/codehaus/cargo/util/DefaultFileHandlerTest.java
@@ -215,20 +215,20 @@ public class DefaultFileHandlerTest
         try
         {
             DefaultFileHandler.validateName("test", "aBc-1.9_XyZ", 5);
-            Assert.fail("Didn't catch length");
+            Assertions.fail("Didn't catch length");
         }
         catch (IllegalArgumentException expected)
         {
-            Assert.assertTrue(expected.getMessage().contains("length"));
+            Assertions.assertTrue(expected.getMessage().contains("length"));
         }
         try
         {
             DefaultFileHandler.validateName("test", "|", 10);
-            Assert.fail("Didn't catch |");
+            Assertions.fail("Didn't catch |");
         }
         catch (IllegalArgumentException expected)
         {
-            Assert.assertTrue(expected.getMessage().contains("Invalid character"));
+            Assertions.assertTrue(expected.getMessage().contains("Invalid character"));
         }
     }
 }
